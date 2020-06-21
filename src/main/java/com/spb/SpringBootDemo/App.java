d0bc47edb5b96cdfd192ef14eb028b83130950a1
ddxx6317 <ddxx5317@163.com>
2020-06-21 12:10:24 +0800

    version

--- src/main/java/com/spb/SpringBootDemo/App.java
+++ src/main/java/com/spb/SpringBootDemo/App.java
@@ -11,12 +11,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling
-public class App 
-{
-	
-	
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
        SpringApplication.run(App.class, args);
     }
 }
